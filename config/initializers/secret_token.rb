2e6861e78ee1bafff97836a27361a5c6c82b46d0
Joel Pineda <joel.pineda@unitec.edu>
2014-03-22 02:30:25 +0000

    First Commit, Create Database and Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DbProject::Application.config.secret_key_base = '650a57c6b2bf7f363e8063aa3d0b4a08f23e121e289f1ef31395b33c45de1e3548f868932bf5fac7b02ee817f23611260f471b4010578d5cf7e8ed69bd8761d6'
